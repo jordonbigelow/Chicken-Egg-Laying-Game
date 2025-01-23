cd1ed4aa46f1c23600fa3f8e9916deed7925cb14
Jordon Bigelow <the-music-dude@hotmail.com>
2025-01-23 12:14:44 -0700

    initial commit, have a moveable chicken in an empty world, some animation done

---
+++ scripts/chicken.gd
@@ -0,0 +1,26 @@
+extends CharacterBody2D
+
+@export var speed: float = 200.0
+
+func _physics_process(delta: float) -> void:
+	var direction: Vector2 = Vector2.ZERO
+	
+	if Input.is_action_pressed("move_left"):
+		direction.x -= 1
+		$AnimatedSprite2D.flip_h = false
+	if Input.is_action_pressed("move_right"):
+		$AnimatedSprite2D.flip_h = true
+		direction.x += 1
+	if Input.is_action_pressed("move_up"):
+		direction.y -= 1
+	if Input.is_action_pressed("move_down"):
+		direction.y += 1
+		
+	if direction.x == 0 && direction.y == 0:
+		$AnimatedSprite2D.play("idle")
+	else:
+		$AnimatedSprite2D.play("walk")
+	
+	direction = direction.normalized()
+	velocity = direction * speed
+	move_and_slide()
