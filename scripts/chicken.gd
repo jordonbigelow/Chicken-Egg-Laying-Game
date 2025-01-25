8b25e7bebabb3937d332cbabee0fc8a358a83d2b
Jordon Bigelow <the-music-dude@hotmail.com>
2025-01-25 16:59:51 -0700

    changed chicken sprite

--- scripts/chicken.gd
+++ scripts/chicken.gd
@@ -7,19 +7,14 @@ func _physics_process(_delta: float) -> void:
 	
 	if Input.is_action_pressed("move_left"):
 		direction.x -= 1
-		$AnimatedSprite2D.flip_h = false
+		$Sprite2D.flip_h = false
 	if Input.is_action_pressed("move_right"):
-		$AnimatedSprite2D.flip_h = true
+		$Sprite2D.flip_h = true
 		direction.x += 1
 	if Input.is_action_pressed("move_up"):
 		direction.y -= 1
 	if Input.is_action_pressed("move_down"):
 		direction.y += 1
-		
-	if direction.x == 0 && direction.y == 0:
-		$AnimatedSprite2D.play("idle")
-	else:
-		$AnimatedSprite2D.play("walk")
 	
 	direction = direction.normalized()
 	velocity = direction * speed
