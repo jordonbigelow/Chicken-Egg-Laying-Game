31019751eded2b825ecdd0bcec71ba14c96d9392
Jordon Bigelow <the-music-dude@hotmail.com>
2025-01-27 09:19:54 -0700

    fixed an error about the flip_h property being set incorrectly. Added an onready var to declare the type of the child sprite2D node explicitly

--- scripts/chicken.gd
+++ scripts/chicken.gd
@@ -2,14 +2,16 @@ extends CharacterBody2D
 
 @export var speed: float = 200.0
 
+@onready var sprite: Sprite2D = $Sprite2D
+
 func _physics_process(_delta: float) -> void:
 	var direction: Vector2 = Vector2.ZERO
 	
 	if Input.is_action_pressed("move_left"):
 		direction.x -= 1
-		$Sprite2D.flip_h = false
+		sprite.flip_h = false
 	if Input.is_action_pressed("move_right"):
-		$Sprite2D.flip_h = true
+		sprite.flip_h = true
 		direction.x += 1
 	if Input.is_action_pressed("move_up"):
 		direction.y -= 1
